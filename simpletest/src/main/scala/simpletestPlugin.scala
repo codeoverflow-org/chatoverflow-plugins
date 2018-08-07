89570d029e4c438f13dc754940d43028eebd94a7
sebinside <hi@sebinside.de>
2018-08-07 20:09:40 +0200

    Finished CLI implementation.

--- simpletest/src/main/scala/simpletestPlugin.scala
+++ simpletest/src/main/scala/simpletestPlugin.scala
@@ -4,14 +4,14 @@ import org.codeoverflow.chatoverflow.api.plugin.{Plugin, PluginManager}
 class simpletestPlugin(manager: PluginManager) extends Plugin {
 
   private val require = new Requirements
-  private val twitchChatInput = require.input.twitchChat("reqTwitch", "A twitch channel", false)
+  //private val twitchChatInput = require.input.twitchChat("reqTwitch", "A twitch channel", false)
   private val nameToSayHelloTo = require.parameter.string("reqHello", "Your name", false)
 
   override def start(): Unit = {
     println("Started successfully!")
     println(s"Dummy message is:${manager.getDummyMessage}")
 
-    twitchChatInput.getValue.registerMessageHandler(msg => println(msg))
+    //twitchChatInput.getValue.registerMessageHandler(msg => println(msg))
 
     println(s"Hello ${nameToSayHelloTo.getValue}!")
 
