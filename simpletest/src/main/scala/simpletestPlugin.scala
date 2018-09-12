dce09df1d1dfe2f1605b2db10269b41c077e9192
Andre Meixner <renxiem@live.de>
2018-09-12 20:17:44 +0200

    Added more complex class library

--- simpletest/src/main/scala/simpletestPlugin.scala
+++ simpletest/src/main/scala/simpletestPlugin.scala
@@ -6,14 +6,20 @@ class simpletestPlugin(manager: PluginManager) extends Plugin {
   private val require = new Requirements
   private val twitchChatInput = require.input.twitchChat("reqTwitch", "A twitch channel", false)
   private val nameToSayHelloTo = require.parameter.string("reqHello", "Your name", false)
+  private val twitchChatOutput = require.output.twitchChat("reqTwitchO", "A twitch channel", false)
 
   override def start(): Unit = {
     println("Started successfully!")
-    println(s"Dummy message is:${manager.getDummyMessage}")
+    println(s"Dummy not loaded msg is:${manager.getDummyMessage}")
 
-    twitchChatInput.getValue.registerMessageHandler(msg => println(msg))
+    twitchChatInput.getValue.registerMessageHandler(msg => println(msg.toHTMLString))
 
     println(s"Hello ${nameToSayHelloTo.getValue}!")
+
+    Thread.sleep(5000)
+
+
+    twitchChatOutput.getValue.sendChatMessage("I am a simple bot message!")
 
     while (true) {
       Thread.sleep(10)
